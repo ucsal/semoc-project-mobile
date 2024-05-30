6ca577a48670e69fd3dae60d948d6061fad1bbf5
Emanuel <101946589+Emanuel3queijos@users.noreply.github.com>
2024-05-30 05:39:21 -0300

    palestrantes view model ok

--- app/src/main/java/com/example/semocavi2/ui/palestrante/PalestrantesViewModel.java
+++ app/src/main/java/com/example/semocavi2/ui/palestrante/PalestrantesViewModel.java
@@ -1,7 +1,35 @@
 package com.example.semocavi2.ui.palestrante;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.semocavi2.models.PalestranteModel;
+import com.example.semocavi2.repo.PalestranteRepository;
+
+import java.util.List;
+
 public class PalestrantesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private PalestranteRepository repository;
+    private LiveData<List<PalestranteModel>> palestrantesLiveDate;
+
+
+    public PalestrantesViewModel(PalestranteRepository repository) {
+        this.repository = repository;
+        this.palestrantesLiveDate = repository.getPalestrante();
+    }
+
+    public LiveData<List<PalestranteModel>> getPalestrantes(){
+        return palestrantesLiveDate;
+
+    }
+    public LiveData<PalestranteModel>getPalestraById(int id){
+        Log.d("id", "o id" + id);
+        return repository.getPalestranteById(id);
+    }
+
+
+
 }
