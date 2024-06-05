51bf7940f7a9fa5a7d42b21e04f1f377d16d336c
Emanuel <101946589+Emanuel3queijos@users.noreply.github.com>
2024-06-05 01:31:36 -0300

    organizando o codigo e deixando legivel para humanos

--- app/src/main/java/com/example/semocavi2/ui/minicurso/MiniCursoDetailsFragment.java
+++ app/src/main/java/com/example/semocavi2/ui/minicurso/MiniCursoDetailsFragment.java
@@ -23,12 +23,11 @@ import androidx.navigation.Navigation;
 
 import com.example.semocavi2.R;
 
+import com.example.semocavi2.models.MiniCursoModel;
 import com.example.semocavi2.ui.notifications.NotificationHelper;
 import com.example.semocavi2.ui.palestrante.PalestrantesViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class MiniCursoDetailsFragment extends Fragment {
 
@@ -38,11 +37,19 @@ public class MiniCursoDetailsFragment extends Fragment {
     private Button buttonInfoPalestrante;
 
 
+    //setando a toolbar
+    private void setupToolbar(View view) {
+        MaterialToolbar materialToolbar = view.findViewById(R.id.materialToolbar);
+        materialToolbar.setNavigationOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            if (!navController.popBackStack()) {
+                navController.navigate(R.id.navigation_minicursos);
+            }
+        });
+    }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mini_curso_details, container, false);
+    //inicializando as views
+    private void initializeViews(View view) {
         titleTextView = view.findViewById(R.id.title);
         descricaoTextView = view.findViewById(R.id.descricaoTextView);
         temaTextView = view.findViewById(R.id.tema);
@@ -53,14 +60,25 @@ public class MiniCursoDetailsFragment extends Fragment {
         horaTextView = view.findViewById(R.id.hora);
         bioTextView = view.findViewById(R.id.instutorBio);
         buttonInfoPalestrante = view.findViewById(R.id.maisInfo);
-        MaterialToolbar materialToolbar = view.findViewById(R.id.materialToolbar);
-        materialToolbar.setNavigationOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(v);
-            if (!navController.popBackStack()) {
-                navController.navigate(R.id.navigation_minicursos);
-            }
-        });
+    }
 
+    //populando as views
+    private void populateMiniCursoData(MiniCursoModel miniCurso, View view) {
+        titleTextView.setText(miniCurso.getNome());
+        descricaoTextView.setText(miniCurso.getDescricao());
+        temaTextView.setText(miniCurso.getTema());
+        localTextView.setText(miniCurso.getLocal());
+        nivelTextView.setText(String.format("Nível: %s", miniCurso.getNivel()));
+        dataTextView.setText(String.format("Data: %s", miniCurso.getData()));
+        horaTextView.setText(String.format("Hora: %s", miniCurso.getHora()));
+        setupNotification(view, miniCurso);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mini_curso_details, container, false);
+        initializeViews(view);
+        setupToolbar(view);
 
         // esse cara esta inicializando as views models relacionados aos fragments, os dados ja foram pegos na main activity
         mViewModel = new ViewModelProvider(requireActivity()).get(MiniCursoViewModel.class);
@@ -68,86 +86,68 @@ public class MiniCursoDetailsFragment extends Fragment {
 // colocando os dados no main fragment
         if (getArguments() != null && getArguments().containsKey("miniCursoId")) {
             int miniCursoId = getArguments().getInt("miniCursoId");
+            observeMiniCursoData(miniCursoId, view);
             mViewModel.getMinicusosById(miniCursoId).observe(getViewLifecycleOwner(), miniCurso -> {
-                if (miniCurso != null) {
-                    titleTextView.setText(miniCurso.getNome());
-                    descricaoTextView.setText(miniCurso.getDescricao());
-                    temaTextView.setText(miniCurso.getTema());
-                    localTextView.setText(miniCurso.getLocal());
-                    nivelTextView.setText(String.format("Nivel: %s", miniCurso.getNivel()));
-                    dataTextView.setText(String.format("Data: %s", miniCurso.getData()));
-                    horaTextView.setText(String.format("Hora: %s", miniCurso.getHora()));
-                    Log.d("palestrante id ", "" + miniCurso.getInstrutorId());
+                populateMiniCursoData(miniCurso, view);
+                observePalestranteData(miniCurso.getInstrutorId(), view);
 
-                    // se tu queria algomais complexo mario, deveria ter deixado mais especifico na atividade😎
-                    ImageView   bellIcon = view.findViewById(R.id.imageView);
-                    bellIcon.setOnClickListener(new
-                                                        View.OnClickListener() {
-                                                            @Override
-                                                            public void onClick(View v) {
-                                                                Context context = getContext();
-                                                                NotificationHelper.createNotificationChannel(context);
-
-                                                                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NotificationHelper.getChannelId())
-                                                                        .setSmallIcon(R.drawable.bell_notification)
-                                                                        .setContentTitle(miniCurso.getNome())
-                                                                        .setContentText("programado para: " + miniCurso.getData())
-                                                                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                                                                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
-                                                                        .setAutoCancel(true);
-
-                                                                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                                                                notificationManager.notify(1, builder.build());
-// aqui eu abre espaco para salvar os minicrusos selecionados dentro de um banco de dados, fazer a verificacao de minicursos disponiveis para salvar e etc. mas como nao foi pedido na atividade vou deixar so isso aqui mesmo
-
-                                                                // so pra nao deixarf clicar dnv
-
-
-                                                                bellIcon.setOnClickListener(null);
-
-                                                            }
-
-                                                        });
-
-                    pViewModel.getPalestraById(miniCurso.getInstrutorId()).observe(getViewLifecycleOwner(), palestrante -> {
-//Se nao tiver id do palestrante, eu coloco o 1 pra representar que e parte do comite de organizacao da semoc, e vai continuar a funcionar mesmo se o professor atualizar o json
-//deixo o botao invisivel caso ele esteja nulo
-                        try {
-                            nomeInstrutorTextView.setText(palestrante.getNome());
-                            bioTextView.setText(palestrante.getBio());
-
-
-                        } catch (NullPointerException e) {
-                            nomeInstrutorTextView.setText("Instrutor Ainda nao Definido");
-                            bioTextView.setText("");
-                            buttonInfoPalestrante.setVisibility(View.GONE);
-
-
-                        }
-
-                        buttonInfoPalestrante.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Bundle bundle = new Bundle();
-                                bundle.putInt("pessoaId", palestrante.getId());
-                                NavController navController = Navigation.findNavController(view);
-                                navController.navigate(R.id.navigation_palestrante, bundle);
-                            }
-                        });
-                    });
-                } else {
-                    Log.d("MiniCursoDetail", "Minicurso not found");
-                }
             });
         } else {
-            Log.d("MiniCursoDetail", "miniCursoId not found in arguments");
+            Log.d("MiniCursoDetail", "miniCursoId nulo");
         }
 
 
         return view;
     }
 
-    private void sendNotification() {
 
+    private void observeMiniCursoData(int miniCursoId, View view) {
+
+    }
+
+    //aqui eu to verificando se os atributos do palestrante vem nulo, ele no caso nao vem nulo inteiro  por conta de alguma coisa maluco que eu fiz na hora de configurar o banco ent eu preciso forcar a exception a acontecer e tratar ela
+    // se tiver nulo eu coloco que ainda nao foi definido um instrutor
+    private void observePalestranteData(int instrutorId, View view) {
+        pViewModel.getPalestraById(instrutorId).observe(getViewLifecycleOwner(), palestrante -> {
+            try {
+                nomeInstrutorTextView.setText(palestrante.getNome());
+                bioTextView.setText(palestrante.getBio());
+                setupPalestranteInfoButton(view, palestrante.getId());
+            } catch (NullPointerException e) {
+                nomeInstrutorTextView.setText("Instrutor Ainda nao Definido");
+                bioTextView.setText("");
+                buttonInfoPalestrante.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setupPalestranteInfoButton(View view, int palestranteId) {
+        buttonInfoPalestrante.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putInt("pessoaId", palestranteId);
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.navigation_palestrante, bundle);
+        });
+    }
+
+    private void setupNotification(View view, MiniCursoModel miniCurso) {
+        ImageView bellIcon = view.findViewById(R.id.imageView);
+        bellIcon.setOnClickListener(v -> {
+            Context context = getContext();
+            NotificationHelper.createNotificationChannel(context);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NotificationHelper.getChannelId())
+                    .setSmallIcon(R.drawable.bell_notification)
+                    .setContentTitle(miniCurso.getNome())
+                    .setContentText("Programado para: " + miniCurso.getData())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
+                    .setAutoCancel(true);
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(1, builder.build());
+
+            bellIcon.setOnClickListener(null);
+        });
     }
 }
